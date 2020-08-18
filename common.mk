e6661a39a99d68ff49f7ad78454b20464ad6ea9e
Demon000 <demonsingur@gmail.com>
2020-08-18 14:56:37 +0300

    sm6250-common: build WiFi packages

--- common.mk
+++ common.mk
@@ -149,6 +149,14 @@ PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
 # WiFi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service \
+    hostapd \
+    libwifi-hal-qcom \
+    libwpa_client \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
