734eb00b492dcfb6a0bc7a301f5ababa599e7b51
Sonal Singh <sonal.singh.19993@gmail.com>
2020-09-11 18:55:08 +0200

    sm6250-common: Include ANXCamera
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -16,6 +16,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
+# ANXCamera
+ifeq ($(SHIP_ANX),true)
+$(call inherit-product, vendor/aeonax/ANXCamera/anx-vendor.mk)
+endif
+
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
