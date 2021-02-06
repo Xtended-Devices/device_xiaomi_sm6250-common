cbb8607d41cdf9bd65bfb079d54b5e0cfd9727fe
SonalSingh18 <sonal.singh.19993@gmail.com>
2021-02-06 18:42:04 +0530

    sm6250-common: arrow -> xtended
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+#rm -rf hardware/qcom-caf/sm8150
+git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-18.1-caf-sm8150 hardware/qcom-caf/sm8150/display
+git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-18.1-caf-sm8150 hardware/qcom-caf/sm8150/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-18.1-caf-sm8150 hardware/qcom-caf/sm8150/media
+git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton
+git clone https://github.com/SonalSingh18/vendor_xiaomi_miuicamera.git -b master vendor/xiaomi/miuicamera
