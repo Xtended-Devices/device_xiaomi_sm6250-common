68db1f5e0796ebd2b3709b5d821b8948c77a14f9
Sonal Singh <sonal.singh.19993@gmail.com>
2021-05-17 18:50:12 +0000

    sm6250-common: arrow -> xtended
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras.git -b arrow-12.0 device/xiaomi/extras
+git clone https://github.com/SonalSingh18/android_packages_apps_GCamGOPrebuilt.git -b arrow-12.0 packages/apps/GCamGOPrebuilt
+git clone https://github.com/ArrowOS-Devices/android_hardware_xiaomi.git -b arrow-12.0 hardware/xiaomi
+git clone https://gitlab.com/SonalSingh18/vendor_xiaomi_miuicamera.git -b 12 vendor/xiaomi/miuicamera
+git clone --depth=1 https://gitlab.com/project-xtended/android_vendor_google.git -b xs vendor/google
+git clone --depth=1 https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b -b master prebuilts/clang/host/linux-x86/clang-r437112b
